da777b88c2ee24e1d6cd5d0414037c5c91506ecc
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2021-04-07 17:29:38 -0500

    heroku deploy

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const cors = require('cors');
 const app = express();
 app.use(cors('*'));
 app.use(express.json());
-app.use(express.urlencoded());
 app.use( require('./src/routes/main'));
 
-const port = 3030 || process.env.PORT;
+const port = process.env.PORT;
 app.listen(port, err => {
     if (err) throw err;
     console.log('Servidor corriendo en el puerto', port);
